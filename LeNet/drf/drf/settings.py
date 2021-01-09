d3c119c79df57ac5b467f99f6ee49ef7faabb9bb
asm <43715819+ami-sm@users.noreply.github.com>
2021-01-09 14:43:11 +0800

    Update settings.py

--- LeNet/drf/drf/settings.py
+++ LeNet/drf/drf/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'koso_mil7138#i+ir(-qf7c@(jryoqkdt8=@!s(=hj^che#=iu'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
